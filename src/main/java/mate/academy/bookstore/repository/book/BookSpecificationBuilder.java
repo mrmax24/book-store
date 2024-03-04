22449114abcf080fef51e87c7b1083aa536e5cd7
Maks Hrytsyna <hrytsynamax@gmail.com>
2024-03-04 13:17:19 +0200

    DI used instead of new

--- src/main/java/mate/academy/bookstore/repository/book/BookSpecificationBuilder.java
+++ src/main/java/mate/academy/bookstore/repository/book/BookSpecificationBuilder.java
@@ -6,27 +6,27 @@ import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.SpecificationBuilder;
 import mate.academy.bookstore.repository.book.spec.AuthorSpecificationProvider;
 import mate.academy.bookstore.repository.book.spec.TitleSpecificationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
-    @Autowired
     private final BookSpecificationProviderManager bookSpecificationProviderManager;
+    private final TitleSpecificationProvider titleSpecificationProvider;
+    private final AuthorSpecificationProvider authorSpecificationProvider;
 
     @Override
     public Specification<Book> build(BookSearchParametersDto searchParametersDto) {
         Specification<Book> spec = Specification.where(null);
         if (searchParametersDto.titles() != null && searchParametersDto.titles().length > 0) {
             spec = spec.and(bookSpecificationProviderManager
-                    .getSpecificationProvider(new TitleSpecificationProvider().getKey())
+                    .getSpecificationProvider(titleSpecificationProvider.getKey())
                     .getSpecification(searchParametersDto.titles()));
         }
         if (searchParametersDto.authors() != null && searchParametersDto.authors().length > 0) {
             spec = spec.and(bookSpecificationProviderManager
-                    .getSpecificationProvider(new AuthorSpecificationProvider().getKey())
+                    .getSpecificationProvider(authorSpecificationProvider.getKey())
                     .getSpecification(searchParametersDto.authors()));
         }
         return spec;
