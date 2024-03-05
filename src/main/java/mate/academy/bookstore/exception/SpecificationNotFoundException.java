2dfe389387eeb1fa8112a57feb44181f39c26c64
Maks Hrytsyna <hrytsynamax@gmail.com>
2024-03-05 16:12:36 +0200

    Ðмфдшіvalidation is added

---
+++ src/main/java/mate/academy/bookstore/exception/SpecificationNotFoundException.java
@@ -0,0 +1,7 @@
+package mate.academy.bookstore.exception;
+
+public class SpecificationNotFoundException extends RuntimeException {
+    public SpecificationNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/mate/academy/bookstore/validation/IsbnValidator.java
@@ -0,0 +1,22 @@
+package mate.academy.bookstore.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class IsbnValidator implements ConstraintValidator<Isbn, String> {
+    private static final String ISBN10_FORMAT =
+            "^\\d{9}[\\d|Xx]|\\d{1,5}-\\d{1,7}-\\d{1,6}-[\\d|Xx]$";
+    private static final String ISBN13_FORMAT =
+            "^(?:\\d{12}\\d|[\\d|-]{1,5}-\\d{1,7}-\\d{1,6}-\\d)$";
+
+    @Override
+    public boolean isValid(String isbn, ConstraintValidatorContext constraintValidatorContext) {
+        return isbn != null && Pattern.matches(ISBN10_FORMAT, isbn)
+                || Pattern.matches(ISBN13_FORMAT, isbn);
+    }
+}
